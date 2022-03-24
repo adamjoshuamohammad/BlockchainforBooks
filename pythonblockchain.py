576839931232d698dd9361285772489bada34d7c
Adam <73608454+adamjoshuamohammad@users.noreply.github.com>
2022-03-24 13:06:55 -0400

    Added Consensus Algorithm
    Created /nodes/register and /nodes/resolve endpoints; implemented consensus algorithm (longest chain)

--- pythonblockchain.py
+++ pythonblockchain.py
@@ -4,16 +4,89 @@ from textwrap import dedent
 from time import time
 from uuid import uuid4
 from flask import Flask, jsonify, request
+from urllib.parse import urlparse
 
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
+        self.nodes=set()
         self.new_block(prev_hash='1', proof=100)
         
     # #Create the genesis block
     # def create_genesis_block(self):
     #     self.new_block(previous_hash=1, proof=100)
+
+    def register_node(self, address):
+        '''
+        Add a new node to the list of nodes
+        :param address: <str> Address of node
+        :return: None
+        '''
+
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
+
+    def valid_chain(self, chain):
+        '''
+        Determine if a given blockchain is valid
+        by verifying the hash of each block
+        :param chain: <list> A blockchain
+        :return: <bool> True if valid, False if not
+        '''
+
+        last_block = chain[0]
+        current_index = 1
+
+        while current_index < len(chain):
+            block = chain[current_index]
+            print(f'{last_block}')
+            print(f'{block}')
+            print("\n-----------\n")
+            #Check that the hash of the block is correct
+            if block['previous_hash'] != self.hash(last_block):
+                return False
+            
+            #Check that the proof of work is correct
+            if not self.valid_proof(last_block['proof'], block['proof']):
+                return False
+            
+            last_block = block
+            current_index += 1
+        
+        return True
+
+    def resolve_conflicts(self):
+        '''
+        This is our consensus algorithm, it resolves conflicts
+        by replacing our chain with the longest one in the network.
+        :return: <bool> True if our chain was replaced, False if not
+        '''
+
+        neighbors = self.nodes
+        new_chain = None
+
+        #We're only looking for chains longer than ours
+        max_length = len(self.chain)
+
+        #Grab and verify the chains from all the nodes in our network
+        for node in neighbors:
+            response = requests.get(f'http://{node}/chain')
+
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+
+                #Check if the length is longer and the chain is valid
+                if length > max_length and self.valid_chain(chain):
+                    max_length = length
+                    new_chain = chain
+
+        if new_chain:
+            self.chain = new_chain
+            return True
+        
+        return False
     
     def new_block(self, proof, prev_hash):
         '''
@@ -176,4 +249,38 @@ def mine():
         'prev_hash': block['prev_hash'],
     }
 
+    return jsonify(response), 200
+
+@app.route('/nodes/register', methods=['POST'])
+def register_nodes():
+    values = request.get_json()
+
+    nodes=values.get('nodes')
+    if nodes is None:
+        return "Error: Please supply a valid list of nodes", 400
+    
+    for node in nodes:
+        blockchain.register_node(node)
+    
+    response = {
+        'message': 'New nodes have been added',
+        'total_nodes': list(blockchain.nodes),
+    }
+    return jsonify(response), 201
+
+@app.route('/nodes/resolve', methods=['GET'])
+def consensus():
+    replaced=blockchain.resolve_conflicts()
+
+    if replaced:
+        response = {
+            'message': 'Our chain was replaced',
+            'new_chain': blockchain.chain
+        }
+    else:
+        response = {
+            'message': 'Our chain is authoritative',
+            'chain': blockchain.chain
+        }
+    
     return jsonify(response), 200
